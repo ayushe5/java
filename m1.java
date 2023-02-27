d759edeb70ad4a4526133969a5d55956cf05c7d2
ayushe5 <125894312+ayushe5@users.noreply.github.com>
2023-02-27 08:53:27 +0530

    Add files via upload

---
+++ m1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+class Volume
+{
+int l;
+int w;
+static int h=0;
+}
+
+class m1
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+m1 v=new m1();
+m1 v1=new m1();
+
+v.l=sc.nextInt;
+System.out.println(v.l);
+}
+}
