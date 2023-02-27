c16b0a7a40eaa7b084ce55c95fda181d6ff4a35b
ayushe5 <125894312+ayushe5@users.noreply.github.com>
2023-02-27 09:21:08 +0530

    Add files via upload

---
+++ sjoin.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class sjoin //join 2 strings without string handling function
+{
+public static void main(String[] args)
+{
+String s1=sc.nextLine();
+String s2=sc.nextLine();
+System.out.println(s1+""+s2);
+}
+}
