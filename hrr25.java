aec3cdf718ac2b591294e1fa1618fc2b3080fd70
ayushe5 <125894312+ayushe5@users.noreply.github.com>
2023-04-29 12:06:12 +0530

    Add files via upload

---
+++ hrr25.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j-1<=n-i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
