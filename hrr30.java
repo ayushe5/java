3ce244084f3b975fbc507ac38a3555ea3ac80d46
ayushe5 <125894312+ayushe5@users.noreply.github.com>
2023-05-01 17:06:41 +0530

    Add files via upload

---
+++ hrr30.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        String arr[]=new String[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.next();
+        
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i].matches(".[0-9]."))
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        System.out.println(-count);
+        System.out.println(n+count);
+    }
+}
